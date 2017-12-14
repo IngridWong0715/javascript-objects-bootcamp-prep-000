3b0ed91f06e42b5fa3cf9c5293c811fc06e2a5a4
Ingrid Wong <yw340@georgetown.edu>
2017-12-14 20:04:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,11 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist;
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+  return playlist;
+}
 /*  it('defines an object called `playlist`', () => {
     expect(typeof playlist).to.equal('object')
     expect(Object.keys(playlist).length).to.be.greaterThan(0)
